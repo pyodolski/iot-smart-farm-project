e0b666e52bb583baea7cd9d8cf85e0b05bdc55ba
pocketopii <rlwns00@gmail.com>
2025-11-20 20:12:23 +0900

    config 수정

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    'host': os.getenv('DB_HOST', 'dpg-d4079k15pdvs73fnmip0-a.oregon-postgres.render.com'),
-    'user': os.getenv('DB_USER', 'smartfarm_user'),
-    'password': os.getenv('DB_PASSWORD', 'QlKgIjMQ3URu7G317WfolWIFatOOiU9n'),
-    'database': os.getenv('DB_NAME', 'smartfarm_seor'),
-    'port': int(os.getenv('DB_PORT', 5432)),  # PostgreSQL 기본 포트
-    'charset': 'utf8mb4'
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'database': os.getenv('DB_NAME'),
+    'port': int(os.getenv('DB_PORT', 5432)),
 }
